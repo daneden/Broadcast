972d3a5e8004a15099f300e98ecf3b5bbe5c78ad
Daniel Eden <dan.eden@me.com>
2021-06-26 20:01:20 +0100

    Initial Commit

---
+++ Broadcast/BroadcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  BroadcastApp.swift
+//  Broadcast
+//
+//  Created by Daniel Eden on 26/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct BroadcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Broadcast/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Broadcast
+//
+//  Created by Daniel Eden on 26/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
