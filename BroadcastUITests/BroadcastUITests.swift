af0062515fd0867c716f2d61c476e1893fdc0230
Daniel Eden <dan.eden@me.com>
2021-08-15 16:33:45 +0100

    Add convenience initialiser for tests

--- BroadcastUITests/BroadcastUITests.swift
+++ BroadcastUITests/BroadcastUITests.swift
@@ -7,24 +7,22 @@
 
 import XCTest
 
-class BroadcastUITests: XCTestCase {
-  
-  override func setUpWithError() throws {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+extension XCUIApplication {
+  static func initWithLaunchParameters() -> XCUIApplication {
+    let instance = XCUIApplication()
+    instance.launchArguments = ["isTestEnvironment"]
     
-    // In UI tests it is usually best to stop immediately when a failure occurs.
-    continueAfterFailure = false
-    
-    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    return instance
   }
-  
-  override func tearDownWithError() throws {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+class BroadcastUITests: XCTestCase {
+  override func setUpWithError() throws {
+    continueAfterFailure = false
   }
   
   func testKeyElementsExist() throws {
-    let app = XCUIApplication()
-    app.launchArguments = ["isTestEnvironment"]
+    let app = XCUIApplication.initWithLaunchParameters()
     app.launch()
     
     let sendTweet = app.buttons["sendTweetButton"]
@@ -36,8 +34,7 @@ class BroadcastUITests: XCTestCase {
   }
   
   func testSendTweetButtonEnabledOnValidText() throws {
-    let app = XCUIApplication()
-    app.launchArguments = ["isTestEnvironment"]
+    let app = XCUIApplication.initWithLaunchParameters()
     app.launch()
     
     let sendTweet = app.buttons["sendTweetButton"]
@@ -54,8 +51,7 @@ class BroadcastUITests: XCTestCase {
   }
   
   func testSendTweetButtonDisabledOnEmptyText() throws {
-    let app = XCUIApplication()
-    app.launchArguments = ["isTestEnvironment"]
+    let app = XCUIApplication.initWithLaunchParameters()
     app.launch()
     
     let sendTweet = app.buttons["sendTweetButton"]
@@ -72,8 +68,7 @@ class BroadcastUITests: XCTestCase {
   }
   
   func testSendTweetButtonDisabledOnLargeText() throws {
-    let app = XCUIApplication()
-    app.launchArguments = ["isTestEnvironment"]
+    let app = XCUIApplication.initWithLaunchParameters()
     app.launch()
     
     let sendTweet = app.buttons["sendTweetButton"]
@@ -89,8 +84,7 @@ class BroadcastUITests: XCTestCase {
   }
   
   func testLogout() throws {
-    let app = XCUIApplication()
-    app.launchArguments = ["isTestEnvironment"]
+    let app = XCUIApplication.initWithLaunchParameters()
     app.launch()
     
     let profilePhoto = app.images["profilePhotoButton"]
